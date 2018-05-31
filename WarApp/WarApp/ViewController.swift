5195e152b33f565c63302673be3654279ca6d31b
Jonathan <jonathan.saavedra08@gmail.com>
2018-05-31 10:32:16 -0400

    Se añade relacion para label y boton entre storyboard y archivo swift

--- WarApp/WarApp/ViewController.swift
+++ WarApp/WarApp/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        print("Deal!")
+        
+    }
+    
 }
 
